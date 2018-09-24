566ead03be2d7f0ef7c03ce66972b1736c43c220
ExtGmrJasonZ <43334472+ExtGmrJasonZ@users.noreply.github.com>
2018-09-24 21:45:42 +0700

    Update Week 01 Assignment - ITP_TA - Looping.py

--- Week 01 Assignment - ITP_TA - Looping.py
+++ Week 01 Assignment - ITP_TA - Looping.py
@@ -1,28 +1,27 @@
-def RightTriangle(num):                                     #print the right angle triangle shaped asterisks
-    x = 0
-    while x <= num:
-        print(" "*(num+x), "*"*(num - x))
-        x += 1
+def RightTriangle(num):                                     # Module for printing the right angle triangle shaped asterisks
+    x = 0                                                   # x starts from 0
+    while x <= num:                                         # this is the while loop in which the condition is x is less than or equal to the value of num
+        print(" "*(num+x), "*"*(num - x))                   # print the spaces to increase down the line and print the asterisks also decrease down the line
+        x += 1                                              # ready to continue to new line
 
-RightTriangle(5)
+RightTriangle(5)                                            # this means that I want to print 5 rows of the module
 
-def Pyramid(num):                                           #print the pyramid shaped asterisks
-    x = 0
-    while x <= num:
-        print(" "*(num-x)+"*"*(x+(x-1)))
-        x += 1
+def Pyramid(num):                                           #Module for printing the pyramid shaped asterisks
+    x = 0                                                   # x starts from 0
+    while x <= num:                                         # this is the while loop in which the condition is x is less than or equal to the value of num
+        print(" "*(num-x)+"*"*(x+(x-1)))                    # print the spaces to decrease down the line and print the asterisks multiply by the value of x + (x - 1)
+        x += 1                                              # ready to continue to new line
 
-Pyramid(5)
+Pyramid(5)                                                  # this means that I want to print 5 rows of the module
 
 
-def Diamond(num):                                           #print the diamond shaped asterisks
-    x = 0
-    while x <= num:
-        print(" "*(num-x)+"*"*(x+(x-1)))                    #code to make the upper triangle
-        x += 1
-    x = 1
-    while x <= num:
-        print(" "*x + "*"*((num - x)*2 - 1))
-        x +=1
+def Diamond(num):                                           # Moduule for printing the diamond shaped asterisks
+    x = 0                                                   # x starts from 0
+    while x <= num:                                         # this is the while loop in which the condition is x is less than or equal to the value of num
+        print(" "*(num-x)+"*"*(x+(x-1)))                    # print the spaces to decrease down the line and print the asterisks multiply by the value of x + (x - 1)               #Upper Triangle
+    x = 1                                                   # x starts from 1
+    while x <= num:                                         # this is the while loop in which the condition is x is less than or equal to the value of num
+        print(" "*x + "*"*((num - x)*2 - 1))                # print the spaces multiply by the value of x added by printing the asterisks multiply by the value of ((num - x)*2 -1) #Lower Triangle
+        x +=1                                               # ready to continue to new line
 
-Diamond(5)
+Diamond(5)                                                  # this means that I want to print 5 rows of the module
